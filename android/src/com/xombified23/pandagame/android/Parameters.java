34bf1201c0a4c8c3d2a336e66f690b2605d2f112
xombified23 <din128@gmail.com>
2015-01-25 22:55:48 -0800

    Removed MAX_NUM_TILES

--- android/src/com/xombified23/pandagame/android/Parameters.java
+++ android/src/com/xombified23/pandagame/android/Parameters.java
@@ -7,7 +7,6 @@ public final class Parameters {
     public final static int TILE_PIXEL_HEIGHT = 216;
     public final static int NUM_MONSTERS = 10;
     public final static int NUM_WALLS = 2;
-    public final static int MAX_NUM_TILES = NUM_X_TILES * NUM_Y_TILES - 3;
     public final static int SCREEN_WIDTH = 1080;
     public final static int SCREEN_HEIGHT = 1920;
     public final static float CHARACTER_SCALE = 0.25f;
